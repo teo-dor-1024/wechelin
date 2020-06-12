b9141f38ffe9bc44abff197fb0174fd8976e25df
teo <teo.dor@kakaocorp.com>
2020-06-12 22:07:05 +0900

    리얼 api 주소로 변경

--- src/util/useMyInfo.js
+++ src/util/useMyInfo.js
@@ -4,17 +4,19 @@ import AsyncStorage from '@react-native-community/async-storage';
 function useMyInfo() {
   const [id, setId] = useState('');
   const [nickName, setNickName] = useState('');
+  const [accessToken, setAccessToken] = useState('');
 
   useEffect(() => {
     const getMyInfo = async () => {
       setId(await AsyncStorage.getItem('id'));
       setNickName(await AsyncStorage.getItem('nickname'));
+      setAccessToken(await AsyncStorage.getItem('token'));
     };
 
     getMyInfo();
   }, []);
 
-  return {id, nickName};
+  return {id, nickName, accessToken};
 }
 
 export default useMyInfo;
