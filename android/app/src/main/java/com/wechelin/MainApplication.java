14eccf1056867592cb958a79d9f9ef063bbf4bec
teo <teo.dor@kakaocorp.com>
2020-06-14 00:13:14 +0900

    안드로이드 - 카카오 로그인, 구글 맵 설정

--- android/app/src/main/java/com/wechelin/MainApplication.java
+++ android/app/src/main/java/com/wechelin/MainApplication.java
@@ -1,5 +1,14 @@
 package com.wechelin;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -44,6 +53,20 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this); // Remove this line if you don't want Flipper enabled
+      try {
+          PackageInfo info = getPackageManager().getPackageInfo(
+                  "com.rn_social_login",
+                  PackageManager.GET_SIGNATURES);
+          for (Signature signature : info.signatures) {
+              MessageDigest md = MessageDigest.getInstance("SHA");
+              md.update(signature.toByteArray());
+              Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+          }
+      } catch (PackageManager.NameNotFoundException e) {
+
+      } catch (NoSuchAlgorithmException e) {
+
+      }
   }
 
   /**
